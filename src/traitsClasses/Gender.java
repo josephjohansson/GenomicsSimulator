c3f4f8c7d2eb7d739a9a703b1f7d30fecf3e375f
tbesenski <tbesenski@hotmail.com>
2014-02-11 16:12:13 -0700

    update gender
    added the lists of dna possibilities for female male and hermaphrodites.
    changed appropriate methods

--- src/traitsClasses/Gender.java
+++ src/traitsClasses/Gender.java
@@ -10,96 +10,44 @@ public class Gender {
 	int geneNumber;
 	List<String> currentEyeColourDNA = new ArrayList<String>();
 	List<String> currentEyeSizeDNA = new ArrayList<String>();
-	List<String> BROWN_EYES_DNA = Arrays.asList("AA","AT","AC","AG","TA","TC","TG");
-	List<String> BLUE_EYES_DNA = Arrays.asList("CA","CT","CC");
-	List<String> GREEN_EYES_DNA = Arrays.asList("GA","GC");
-	List<String> HAZEL_EYES_DNA = Arrays.asList("GG","GT");
-	List<String> GREY_EYES_DNA = Arrays.asList("TT");
-	List<String> PURPLE_EYES_DNA = Arrays.asList("CG");
-	List<String> SMALL_EYES_DNA = Arrays.asList("A");
-	List<String> MEDIUM_EYES_DNA = Arrays.asList("T","C");
-	List<String> LARGE_EYES_DNA = Arrays.asList("G");
+	List<String> MALE_DNA= Arrays.asList("AA", "AG", "AC", "AG", "GA", "GC", "GT");
+	List<String> FEMALE_DNA = Arrays.asList("CA", "CT", "CG", "TT", "TC", "TG", "TA");
+	List<String> HERMAPHRODITE_DNA = Arrays.asList("GG","CC");
+
 
 
 	
 	
 //----------------------------------------------------------------------------------------------------
 //PHENOTYPE INPUT GETS POSSIBLE GENE LIST
-	// Determines DNA code from given eye colour	
+	// Determines DNA code from given gender	
 	public void getEyeColour(String eyesColour){
-		if (eyesColour.equals("brown")){
-			currentEyeColourDNA = BROWN_EYES_DNA;
+		if (gender.equals("male")){
+			currentGenderDNA = MALE_DNA;
 		}
-		else if (eyesColour.equals("blue")){
-			currentEyeColourDNA = BLUE_EYES_DNA;
+		else if (gender.equals("female")){
+			currentGenderDNA = FEMALE_DNA;
 		}
-		else if (eyesColour.equals("green")){
-			currentEyeColourDNA = GREEN_EYES_DNA;
-		}
-		else if (eyesColour.equals("hazel")){
-			currentEyeColourDNA = HAZEL_EYES_DNA;
-		}
-		else if (eyesColour.equals("grey")){
-			currentEyeColourDNA = GREY_EYES_DNA;
-		}
-		else if (eyesColour.equals("purple")){
-			currentEyeColourDNA = PURPLE_EYES_DNA;
+		else if (gender.equals("hermaphrodite")){
+			currentGenderDNA = HERMAPHRODITE_DNA;
 		}
 	}
-	
-	// Determines DNA code from given eye size -- to be randomized 
-	void getSize(String eyesSize){
-		if (eyesSize.equals("small")){
-			currentEyeSizeDNA = SMALL_EYES_DNA;
-		}
-		else if (eyesSize.equals("medium")){
-			currentEyeSizeDNA = MEDIUM_EYES_DNA;
-		} 
-		else if (eyesSize.equals("large")){
-			currentEyeSizeDNA = LARGE_EYES_DNA;
-		}		
-	}
-
-	
-	
-	
 	
 //-----------------------------------------------------------------------------------------------------
 //DNA INPUT PHENOTYPE OUTPUT
 	// Determines eye colour phenotype from given eye colour DNA gene	
-	void printOutEyeColour(String eyeColourDNA){
-		if (BROWN_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("BROWN");
+	void printOutGender(String genderDNA){
+		if (MALE_DNA.contains(genderDNA)){
+			System.out.println("male");
 		}
-		else if (BLUE_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("BLUE");
+		else if (FEMALE_DNA.contains(genderDNA)){
+			System.out.println("female");
 		}
-		else if (GREEN_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("GREEN");
-		}
-		else if (HAZEL_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("HAZEL");
-		}
-		else if (GREY_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("GREY");
-		}
-		else if (PURPLE_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("PURPLE");
-		}
-	}
-	void printOutEyeSize(String eyeSizeDNA){
-		if (SMALL_EYES_DNA.contains(eyeSizeDNA)){
-			System.out.println("SMALL");
-		}
-		else if (MEDIUM_EYES_DNA.contains(eyeSizeDNA)){
-			System.out.println("MEDIUM");
-		}
-		else if (LARGE_EYES_DNA.contains(eyeSizeDNA)){
-			System.out.println("LARGE");
+		else if (HERMAPHRODITE_DNA.contains(genderDNA)){
+			System.out.println("hermaphrodite");
 		}
 	}
 
-	
 	
 	
 	
