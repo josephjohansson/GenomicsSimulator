d8c4269b542799bbae91f255494371d963e845b1
AreeshaSalman <areesha.s10148@gmail.com>
2014-02-12 14:55:09 -0700

    Finished Face class

--- src/traitsClasses/Face.java
+++ src/traitsClasses/Face.java
@@ -8,56 +8,40 @@ public class Face {
 	String currentGene;
 	int currentDNALength;
 	int geneNumber;
-	List<String> currentEyeColourDNA = new ArrayList<String>();
-	List<String> currentEyeSizeDNA = new ArrayList<String>();
-	List<String> BROWN_EYES_DNA = Arrays.asList("AA","AT","AC","AG","TA","TC","TG");
-	List<String> BLUE_EYES_DNA = Arrays.asList("CA","CT","CC");
-	List<String> GREEN_EYES_DNA = Arrays.asList("GA","GC");
-	List<String> HAZEL_EYES_DNA = Arrays.asList("GG","GT");
-	List<String> GREY_EYES_DNA = Arrays.asList("TT");
-	List<String> PURPLE_EYES_DNA = Arrays.asList("CG");
-	List<String> SMALL_EYES_DNA = Arrays.asList("A");
-	List<String> MEDIUM_EYES_DNA = Arrays.asList("T","C");
-	List<String> LARGE_EYES_DNA = Arrays.asList("G");
+	List<String> currentNoseSizeDNA = new ArrayList<String>();
+	List<String> currentEarLobeDNA = new ArrayList<String>();
+	List<String> SMALL_NOSE_DNA = Arrays.asList("A");
+	List<String> MEDIUM_NOSE_DNA = Arrays.asList("G","C");
+	List<String> LARGE_NOSE_DNA = Arrays.asList("T");
+	List<String> ATTACHED_EAR_LOBE_DNA = Arrays.asList("A", "G");
+	List<String> DETACHED_EAR_LOBE_DNA = Arrays.asList("C", "T");
 
 
 	
 	
 //----------------------------------------------------------------------------------------------------
 //PHENOTYPE INPUT GETS POSSIBLE GENE LIST
-	// Determines DNA code from given eye colour	
-	public void getEyeColour(String eyesColour){
-		if (eyesColour.equals("brown")){
-			currentEyeColourDNA = BROWN_EYES_DNA;
+	// Determines DNA code from given nose size	
+	public void getNoseSize(String noseSize){
+		if (noseSize.equals("small")){
+			currentNoseSizeDNA = SMALL_NOSE_DNA;
 		}
-		else if (eyesColour.equals("blue")){
-			currentEyeColourDNA = BLUE_EYES_DNA;
+		else if (noseSize.equals("medium")){
+			currentNoseSizeDNA = MEDIUM_NOSE_DNA;
 		}
-		else if (eyesColour.equals("green")){
-			currentEyeColourDNA = GREEN_EYES_DNA;
-		}
-		else if (eyesColour.equals("hazel")){
-			currentEyeColourDNA = HAZEL_EYES_DNA;
-		}
-		else if (eyesColour.equals("grey")){
-			currentEyeColourDNA = GREY_EYES_DNA;
-		}
-		else if (eyesColour.equals("purple")){
-			currentEyeColourDNA = PURPLE_EYES_DNA;
+		else if (noseSize.equals("large")){
+			currentNoseSizeDNA = LARGE_NOSE_DNA;
 		}
 	}
 	
-	// Determines DNA code from given eye size -- to be randomized 
-	void getSize(String eyesSize){
-		if (eyesSize.equals("small")){
-			currentEyeSizeDNA = SMALL_EYES_DNA;
+	// Determines DNA code from given ear lobe 
+	void getEarLobe(String earLobe){
+		if (earLobe.equals("attached")){
+			currentEarLobeDNA = ATTACHED_EAR_LOBE_DNA;
 		}
-		else if (eyesSize.equals("medium")){
-			currentEyeSizeDNA = MEDIUM_EYES_DNA;
-		} 
-		else if (eyesSize.equals("large")){
-			currentEyeSizeDNA = LARGE_EYES_DNA;
-		}		
+		else if (earLobe.equals("detached")){
+			currentEarLobeDNA = DETACHED_EAR_LOBE_DNA;
+		} 		
 	}
 
 	
@@ -66,36 +50,24 @@ public class Face {
 	
 //-----------------------------------------------------------------------------------------------------
 //DNA INPUT PHENOTYPE OUTPUT
-	// Determines eye colour phenotype from given eye colour DNA gene	
-	void printOutEyeColour(String eyeColourDNA){
-		if (BROWN_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("BROWN");
-		}
-		else if (BLUE_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("BLUE");
-		}
-		else if (GREEN_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("GREEN");
-		}
-		else if (HAZEL_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("HAZEL");
-		}
-		else if (GREY_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("GREY");
-		}
-		else if (PURPLE_EYES_DNA.contains(eyeColourDNA)){
-			System.out.println("PURPLE");
-		}
-	}
-	void printOutEyeSize(String eyeSizeDNA){
-		if (SMALL_EYES_DNA.contains(eyeSizeDNA)){
+	// Determines nose size phenotype from given nose size DNA gene	
+	void printOutEyeColour(String noseSizeDNA){
+		if (SMALL_NOSE_DNA.contains(noseSizeDNA)){
 			System.out.println("SMALL");
 		}
-		else if (MEDIUM_EYES_DNA.contains(eyeSizeDNA)){
+		else if (MEDIUM_NOSE_DNA.contains(noseSizeDNA)){
 			System.out.println("MEDIUM");
 		}
-		else if (LARGE_EYES_DNA.contains(eyeSizeDNA)){
+		else if (LARGE_NOSE_DNA.contains(noseSizeDNA)){
 			System.out.println("LARGE");
+		}
+	}
+	void printOutEarLobe(String earLobeDNA){
+		if (ATTACHED_EAR_LOBE_DNA.contains(earLobeDNA)){
+			System.out.println("ATTACHED");
+		}
+		else if (DETACHED_EAR_LOBE_DNA.contains(earLobeDNA)){
+			System.out.println("DETACHED");
 		}
 	}
 
