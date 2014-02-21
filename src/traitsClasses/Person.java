1343740df0d9e9f008a3e2713684c894b7c018db
Jessica <Jessica@10.11.69.31>
2014-02-20 22:45:32 -0700

    -Added a new "TextFile" class that creates, appends to, or reads a file 	-Creates a new file when you enter a new name 	-Only appends when the file name already exists -Added a name variable to be passed into "Person" and "TextFiles" for naming (we should have a "Name" variable/class shouldn't we haha?) -Added TextFile methods to "Person" class to execute -Removed old "tester" with new "Test" class in main

--- src/traitsClasses/Person.java
+++ src/traitsClasses/Person.java
@@ -1,16 +1,26 @@
 package traitsClasses;
 
+import Main.TextFile;
+
 public class Person {
-	public void getUserInputOnTraits(String userInputEye, String userInputHair){
+	public void getUserInputOnTraits(String nameInput, String eyeInput, String hairInput){
+		String name = nameInput;
+		TextFile DNA = new TextFile();
 		Eyes eyes = new Eyes();
 		Hair hair = new Hair();
-		eyes.getEyeColour(userInputEye);
+		eyes.getEyeColour(eyeInput);
 		System.out.println(eyes.currentEyeColourDNA);
 		eyes.getRandomGene(eyes.currentEyeColourDNA);
 		System.out.println(eyes.currentGene);
-		hair.getHairColour(userInputHair);
+		hair.getHairColour(hairInput);
 		System.out.println(hair.currentHairColourDNA);
 		hair.getRandomGene(hair.currentHairColourDNA);
 		System.out.println(hair.currentGene);
+		
+		DNA.Create(name);
+		DNA.Append(name,eyes.currentGene);
+		DNA.Append(name,hair.currentGene);
+		
+		DNA.Reader(name);
 	}	
 }
