ef90519d5197606b8f50a98f50d3d04891cd12bb
rodrigjg <rodrigjg@ucalgary.ca>
2014-02-25 13:50:38 -0700

    working on babyTester

--- src/Tests/babyTester.java
+++ src/Tests/babyTester.java
@@ -2,14 +2,33 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+import java.util.UUID;
+
 import org.junit.Test;
+
 import BabyCreation.BabyRandomizer;
 
 public class babyTester {
 
 	@Test
 	public void testGenerateChildsTrait() {
-		BabyRandomizer.generateChildsTrait(mothersTraitChance, fathersTraitChance, mothersTrait, fathersTrait)
+		Random generator = new Random();
+		for (int stringOneRuns = 1; stringOneRuns<10; stringOneRuns++){
+			String inputOne = UUID.randomUUID().toString();
+			for(int stringTwoRuns = 1; stringTwoRuns<10; stringTwoRuns++){
+				String inputTwo = UUID.randomUUID().toString();
+				for (int intOneRuns = 1; intOneRuns<10; intOneRuns++){
+					int intInputOne = generator.nextInt();
+					for (int intTwoRuns = 1; intTwoRuns<10; intTwoRuns++){
+						int intInputTwo = generator.nextInt();
+						String actualTrait = BabyRandomizer.generateChildsTrait(intInputOne, intInputTwo, inputOne, inputTwo);
+						String expectedTraits
+					}
+				}
+			}
+		}
+		
 	}
 
 }
